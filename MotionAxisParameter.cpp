cc22e9efea3d49dfec5c18fd1d540609a6161362
Wouter Hulsbergen <wouter.hulsbergen@gmail.com>
2017-12-04 08:30:06 +0100

    introduced MotionAxisParameter

---
+++ MotionAxisParameter.cpp
@@ -0,0 +1,56 @@
+#include "MotionAxisParameter.h"
+#include "MotionSystemSvc.h"
+#include "MotionAxis.h"
+#include "MotionSystemCommandLibrary.h"
+#include <QDebug>
+
+namespace PAP
+{
+  MotionAxisParameter::MotionAxisParameter(MotionAxis& axis,
+					   const MSCommandLibrary::ParameterDefinition& pardef)
+    :
+    m_axis{&axis},
+    m_pardef{&pardef},
+    m_setValue{axis.name() + "." + pardef.name, pardef.type},
+    m_getValue{pardef.type},
+    m_initialized(false)
+  {
+    //QObject::connect(&m_setValue,&ValueChangedEmitter::valueChanged,[&](){ this->write() ; }) ; 
+    //QObject::connect(&m_setValue,&ValueChangedEmitter::valueChanged,this,&MotionAxisParameter::write) ;
+    QObject::connect(&m_setValue,&NamedVariant::valueChanged,this,&MotionAxisParameter::write) ;
+    qDebug() << "In MAP constructor: " << m_setValue.name() << &m_setValue ;
+  }
+  
+  void MotionAxisParameter::write()
+  {
+    qDebug() << "MotionAxisParameter::write"
+	     << m_setValue.value()
+	     << m_getValue.value() ;
+    if( m_setValue.value() != m_getValue.value() ) {
+      auto id = m_axis->id() ;
+      switch(m_pardef->type) {
+      case QVariant::Double:
+	MotionSystemSvc::instance()->applyAxisCommand(id,m_pardef->setcmd,m_setValue.value().toDouble()) ;
+	break;
+      case QVariant::String:
+	MotionSystemSvc::instance()->applyAxisCommand(id,m_pardef->setcmd,m_setValue.toString().toUtf8().constData()) ;
+	break;
+      default:
+	qDebug() << "applyParameter: Don't know what to do with this parameter type: " << m_pardef->type ;
+      }
+      // update the getvalue
+      read() ;
+    }
+  }
+  
+  void MotionAxisParameter::read()
+  {
+    qDebug() << "Reading motion axis parameter "
+	     << m_setValue.name() << &m_setValue << this ;
+    MotionSystemSvc::instance()->readAxisVariable(m_axis->id(),m_pardef->getcmd,m_getValue) ;
+    if(!m_initialized) {
+      m_setValue.setWithoutSignal(m_getValue.value()) ;
+      m_initialized = true ;
+    }
+  }
+}
---
+++ MotionAxisParameter.h
@@ -0,0 +1,51 @@
+#ifndef MOTIONAXISPARAMETER
+#define MOTIONAXISPARAMETER
+
+#include "NamedValue.h"
+
+namespace PAP
+{
+  class MotionAxis ;
+  namespace MSCommandLibrary {
+    struct ParameterDefinition ;
+  }
+  
+  //template<typename T>
+  class MotionAxisParameter : public QObject
+  {
+    Q_OBJECT
+  public:
+    using SetValueType = NamedVariant ;
+    using GetValueType = MonitoredValue<QVariant> ;
+    MotionAxisParameter(MotionAxis& axis,
+			const MSCommandLibrary::ParameterDefinition& pardef) ;
+
+    SetValueType& setValue() { return m_setValue ; }
+    GetValueType& getValue() { return m_getValue ; }
+    const SetValueType& setValue() const { return m_setValue ; }
+    const GetValueType& getValue() const { return m_getValue ; }
+    const MSCommandLibrary::ParameterDefinition& pardef() const { return *m_pardef ; }
+    
+    // this bypasses the other set method of NamedValue. it
+    // immediately sends an update command to the motion system
+    //void setParameter( const ValueType& value, bool autoupdate = true ) ;
+    // explicitly called 
+    //void getParameter() ;
+
+  public slots:
+    void read() ;
+    void write() ;
+  private:
+    const MotionAxis* m_axis ;
+    const MSCommandLibrary::ParameterDefinition* m_pardef ;
+    // this is the value that we send to the motion system
+    SetValueType m_setValue ;
+    // this is the value that we read back from the motion system
+    GetValueType m_getValue ;
+    // is the set value actually initialized?
+    bool m_initialized ;
+  } ;
+}
+
+
+#endif // MOTIONAXISPARAMETERS_H
