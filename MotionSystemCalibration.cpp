d7f014da4624d1fdc0d348bf6ee6ed7f6eaf7fd3
Wouter Hulsbergen <wouterh@nikhef.nl>
2018-03-15 10:22:41 +0100

    updated stack axis calibration

--- MotionSystemCalibration.cpp
+++ MotionSystemCalibration.cpp
@@ -4,11 +4,14 @@
 #include <QSpinBox>
 #include <QPushButton>
 #include <QTextEdit>
+#include <QDebug>
+
 #include "GeometrySvc.h"
 #include "MotionSystemCalibration.h"
 #include "CoordinateMeasurement.h"
 #include "MotionSystemSvc.h"
 #include "Eigen/Dense"
+#include <iostream>
 
 namespace PAP
 {
@@ -91,7 +94,7 @@ namespace PAP
       connect( addbutton, &QPushButton::clicked, [=](){ this->add() ; } ) ;
       hlayout->addWidget( addbutton ) ;
 
-       m_measurementsdisplaybox = new QTextEdit{this} ;
+      m_measurementsdisplaybox = new QTextEdit{this} ;
       layout->addWidget( m_measurementsdisplaybox ) ;
       m_measurementsdisplaybox->resize(500,100) ;
 
@@ -111,7 +114,8 @@ namespace PAP
       hlayout2->addWidget( readybutton ) ;
 
       // need something to insert a new measurement
-      // need a button to clear the list of measurements, to calibrate, and to test     
+      // need a button to clear the list of measurements, to calibrate, and to test
+   
     }
         
     void updatePositionLabel()
@@ -280,11 +284,14 @@ namespace PAP
 
   class StackCalibrationPage : public QWidget
   {
+  private:
+    QPointF m_taggedCameraInStack ;
+    std::vector<MSCoordinates> m_measurements ;
   public:
     StackCalibrationPage( QWidget* parent=0 )
       : QWidget( parent )
     {
-      auto hlayout = new QHBoxLayout{} ;
+      auto hlayout = new QVBoxLayout{} ;
       this->setLayout(hlayout) ;
 
       auto tmptext1 = new QLabel{this} ;
@@ -294,7 +301,256 @@ namespace PAP
       tmptext1->setWordWrap(true);
       hlayout->addWidget( tmptext1 ) ;
 
+      auto recordbutton = new QPushButton{"Record"} ;
+      hlayout->addWidget(recordbutton) ;
+      connect(recordbutton,&QPushButton::clicked,
+	      [=]{ this->m_measurements.push_back( MotionSystemSvc::instance()->coordinates() ) ; } ) ;
+
+      auto clearbutton = new QPushButton{"Clear"} ;
+      hlayout->addWidget(clearbutton) ;
+      connect(clearbutton,&QPushButton::clicked,
+	      [=]{ this->m_measurements.clear() ; } ) ;
+
+      auto calibratebutton = new QPushButton{"Calibrate"} ;
+      hlayout->addWidget(calibratebutton) ;
+      connect(calibratebutton,&QPushButton::clicked,
+	      [=]{ this->calibrate() ; } ) ;
+      
+      auto tagbutton = new QPushButton{"Tag"} ;
+      hlayout->addWidget(tagbutton) ;
+      connect(tagbutton,&QPushButton::clicked,
+	      [=]{ this->tag() ; } ) ;
+
+      auto trackbutton = new QPushButton{"Track"} ;
+      hlayout->addWidget(trackbutton) ;
+      connect(trackbutton,&QPushButton::clicked,
+	      [=]{ this->track() ; } ) ;
+      /*
+      {
+	// make a few buttons for predefined positions, such that this takes me less time
+	auto mssvc = MotionSysteSvc::instance() ;
+	auto stepbuttonlayout = new QHBoxLayout{} ;
+	hlayout->addLayout(stepbuttonlayout) ;
+	auto P1 = new QPushButton{"P1"} ;
+	stepbuttonlayout->addWidget(P1) ;
+	connext(P1,&QPushButton::clicked,
+		[=]{ mssvc
+	
+
+      }
+      */
     }
+
+    // calibrate from a number of measurements
+    void calibrate()
+    {
+      qDebug() << "Number of measurements: " << m_measurements.size() ;
+      // below we use transforms to go from one frame to another. but
+      // to compute the parameters we need to explicitly model the
+      // transforms, compute derivatives etc. in that process, we
+      // hopefully also find the mistake in the transforms.
+
+      // This is the transformation 'geomsvc::fromCameratoGlobal. We
+      // do not touch that in this calibration step. Let's assume it is right.
+      //  QTransform rc ;
+      //  MSMainCoordinates main = MotionSystemSvc::instance()->maincoordinates() ;
+      //  Coordinates2D offset = toGlobal( main ) ;
+      //  rc.rotateRadians( m_cameraPhi ) ;
+      //  rc.translate( offset.x(), offset.y() ) ;
+
+      // This computes the transform for the stack axis.
+      /*
+	Coordinates2D GeometrySvc::stackAxisInGlobal( const MSStackCoordinates& c ) const
+	{
+ 	 return Coordinates2D{
+	  m_stackX0 + m_stackXA * c.x + m_stackXB * c.y,
+	  m_stackY0 + m_stackYA * c.x + m_stackYB * c.y,
+	  m_stackPhi0 + c.phi
+	 } ; 
+	}
+	QTransform GeometrySvc::fromStackToGlobal() const
+	{
+	Coordinates2D coord = stackAxisInGlobal() ;
+	QTransform rc ;
+	rc.translate( coord.x(), coord.y() ) ;
+	rc.rotateRadians( -coord.phi() ) ; // I do not know why I need the minus sign here :-(
+	return rc ;
+	}
+
+      */
+
+      // In the following we need to compute stackX?, stackY? and
+      // stackPhi0. These are 7 parameters in total. However, we
+      // assume that the X and Y axis scale are well
+      // calibrated, so that we only need to calibrate the angles of both axis with the global system axes.
+
+      auto geomsvc = GeometrySvc::instance() ;
+      if(m_measurements.size()>0) {
+	// for every measurement we do
+	// - compute the global coordinates from MSMain coordinates
+	// - compute the prediction of this by using the transforms
+	// - compute the residuals to the transform parameters
+
+	// the first measurement has zero residual: this is what
+	// defines the marker in the stack frame. but it also cannot
+	// be right to exclude it from the measurement, can it?
+
+	// but of course: the position of the marker in the stack
+	// frame is also unknown: so these are two extra
+	// parameters. we need to initialize them with something, so
+	// we do that with the first measurement.
+
+	// compute the markerposition in the stack frame from the first measurement.
+	QPointF markerposstackframe ;
+	{
+	  const auto m0 =  m_measurements.front() ;
+	  const Coordinates2D markerglobalpos = geomsvc->toGlobal( m0.main ) ;
+	  const Coordinates2D stackaxis = geomsvc->stackAxisInGlobal( m0.stack ) ;
+	  const double dx = markerglobalpos.x() - stackaxis.x() ;
+	  const double dy = markerglobalpos.y() - stackaxis.y() ;
+	  // now just rotate in the stack frame: need to get the sign of the angle right:-)
+	  const double sinphi = std::sin(stackaxis.phi()) ;
+	  const double cosphi = std::cos(stackaxis.phi()) ;
+	  double dxprime = cosphi*dx - sinphi*dy ; 
+	  double dyprime = sinphi*dx + cosphi*dy ; 
+	  markerposstackframe = QPointF{dxprime,dyprime} ;
+	  qDebug() << "Position of marker in stack frame: " << markerposstackframe ;
+	}
+	// the parameters are:
+	// 0: marker pos stack frame x
+	// 1: marker pos stack frame y
+	// 2: m_stackX0
+	// 3: m_stackY0
+	// 4. thetaX: m_stackXA = cos(thetaX); m_stackYA = sin(thetaX) 
+	// 5. thetaY: m_stackXB = sin(thetaY); m_stackYB = cos(thetaY) 
+	// 6: m_stackPhi0
+	using Vector7d = Eigen::Matrix<double,7,1> ;
+	using Matrix7d = Eigen::Matrix<double,7,7> ;
+	Vector7d pars ;
+	pars(0) = markerposstackframe.x() ;
+	pars(1) = markerposstackframe.y() ;
+	pars(2) = geomsvc->stackX0() ;
+	pars(3) = geomsvc->stackY0() ;
+	pars(4) = std::atan2(geomsvc->stackYA(),geomsvc->stackXA()) ;
+	pars(5) = std::atan2(geomsvc->stackXB(),geomsvc->stackYB()) ;
+	pars(6) = geomsvc->stackPhi0() ;
+
+	
+					    
+	const int maxnsteps = 5 ; 
+	for( int istep=0; istep<maxnsteps; ++istep) {
+
+	  Vector7d halfdchi2dpar   = Vector7d::Zero() ;
+	  Matrix7d halfd2chi2dpar2 = Matrix7d::Zero() ;
+	  const double sigma2 = 0.005*0.005 ; // sigma = 5 micron
+	  
+	  for( const auto& m : m_measurements) {
+	    // now we do the same, but with inverse transformations, compute residual, etc
+	    Coordinates2D markerglobalposref = geomsvc->toGlobal( m.main ) ;
+
+	    const Coordinates2D stackaxisorig = geomsvc->stackAxisInGlobal( m.stack ) ;
+	    const Coordinates2D stackaxis = {
+	      pars(2) + std::cos( pars(4) ) * m.stack.x + std::sin( pars(5) ) * m.stack.y,
+	      pars(3) + std::sin( pars(4) ) * m.stack.x + std::cos( pars(5) ) * m.stack.y,
+	      pars(6) + m.stack.phi } ;
+	    qDebug() << "stackaxis orig: "
+		     << stackaxisorig.x() << stackaxisorig.y() <<  stackaxisorig.phi() ;
+	    qDebug() << "stackaxis new : "
+		     << stackaxis.x() << stackaxis.y() <<  stackaxis.phi() ;
+	    
+	    	    
+	    const double sinphi = std::sin(stackaxis.phi()) ;
+	    const double cosphi = std::cos(stackaxis.phi()) ;
+	    double x = stackaxis.x() + pars(0) * cosphi + pars(1) * sinphi ;
+	    double y = stackaxis.y() - pars(0) * sinphi + pars(1) * cosphi ;
+
+	    double residualX = x - markerglobalposref.x() ;
+	    double residualY = y - markerglobalposref.y() ;
+	    qDebug() << "Residual: " << residualX << residualY ;
+	    
+	    // x and y factorise, so just do them one by one
+	    Vector7d deriv ;
+	    {
+	      // derivatives of residual X
+	      deriv(0) = cosphi ;
+	      deriv(1) = sinphi ;
+	      deriv(2) = 1 ;
+	      deriv(3) = 0 ;
+	      deriv(4) = -1.0 * std::sin( pars(4) ) * m.stack.x ;
+	      deriv(5) = +1.0 * std::cos( pars(5) ) * m.stack.y ;
+	      deriv(6) = pars(0) * -1.0 * sinphi + pars(1) * +1.0 * cosphi ;
+	      halfdchi2dpar += residualX/sigma2 * deriv ;
+	      for(int irow=0; irow<7; ++irow)
+		for(int icol=0; icol<7; ++icol)
+		  halfd2chi2dpar2(irow,icol) += deriv(irow)*deriv(icol)  / sigma2;
+	    }
+	    {
+	      // derivatives of residual Y
+	      deriv(0) = -sinphi ;
+	      deriv(1) =  cosphi ;
+	      deriv(2) = 0 ;
+	      deriv(3) = 1 ;
+	      deriv(4) = +1.0 * std::cos( pars(4) ) * m.stack.x ;
+	      deriv(5) = -1.0 * std::sin( pars(5) ) * m.stack.y ;
+	      deriv(6) = pars(0) * -1.0 * cosphi + pars(1) * -1.0 * sinphi ;
+	      halfdchi2dpar += residualY/sigma2* deriv ;
+	      for(int irow=0; irow<7; ++irow)
+		for(int icol=0; icol<7; ++icol)
+		  halfd2chi2dpar2(irow,icol) += deriv(irow)*deriv(icol)  / sigma2;
+	    }
+	  }
+	  
+	  std::cout << "1st derivative: " << halfdchi2dpar << std::endl ;
+	  std::cout << "2nd derivative: " << halfd2chi2dpar2 << std::endl ;
+		  
+	  Vector7d delta = halfd2chi2dpar2.ldlt().solve(halfdchi2dpar) ;
+	  std::cout << "Delta: " << delta << std::endl ;
+	  double dchi2 = delta.dot(halfdchi2dpar) ;
+	  
+	  std::cout << "deltachi2 (module factor 2): " << istep << " " << delta.dot(halfdchi2dpar) << std::endl ;
+	  
+	  pars -= delta ;
+	  if(dchi2<0.01) break ;
+	  
+	}
+	geomsvc->setStackParameters( pars(2), std::cos( pars(4) ), std::sin( pars(5) ),
+				     pars(3), std::sin( pars(4) ), std::cos( pars(5) ),
+				     pars(6) ) ;
+	m_measurements.clear() ;
+      }
+    }
+
+    // determine the current global position of the camera in the stack frame
+    void tag()
+    {
+      auto geomsvc = GeometrySvc::instance() ;
+      QTransform fromCameraToGlobal = geomsvc->fromCameraToGlobal() ;
+      QTransform fromStackToGlobal = geomsvc->fromStackToGlobal() ;
+      QTransform fromCameraToStack =  fromCameraToGlobal * fromStackToGlobal.inverted();
+      
+      //QPointF cameraInGlobal = QPointF(0,0) * fromCameraToGlobal ;
+      QPointF cameraInStack  = QPointF(0,0) * fromCameraToStack ;
+      qDebug() << "CameraInStack: " << cameraInStack ;
+      m_taggedCameraInStack = cameraInStack ;
+    } ;
+
+    // move the camera to the tagged position
+    void track()
+    {
+      //
+      auto geomsvc = GeometrySvc::instance() ;
+      QTransform fromStackToGlobal = geomsvc->fromStackToGlobal() ;
+      QPointF globalpos = m_taggedCameraInStack * fromStackToGlobal ;
+      qDebug() << "In track: " << globalpos ;
+      // we must find a better way to do this
+      auto msmaincoordinates = geomsvc->toMSMain( globalpos ) ;
+      auto mssvc = MotionSystemSvc::instance() ;
+      mssvc->mainXAxis().moveTo( msmaincoordinates.x ) ;
+      mssvc->mainYAxis().moveTo( msmaincoordinates.y ) ;
+      // Current status: this orks for x,y displacements, but rotation
+      // is exactly in opposite direction!
+    } ;
+    
   } ;
 
 
