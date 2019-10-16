ac184cad69df1328f91006563de2a0224b80fa00
Wouter Hulsbergen <wouter.hulsbergen@gmail.com>
2019-10-16 11:38:35 +0200

    added PhotoBooth

--- MetrologyPages.h
+++ MetrologyPages.h
@@ -14,6 +14,9 @@ namespace PAP
   QWidget* createSubstrateSurfaceMetrologyPage(CameraWindow& camerasvc, ViewDirection viewdir) ;
   QWidget* createGenericSurfaceMetrologyPage(CameraWindow& camerasvc, ViewDirection viewdir) ;
   QTabWidget* createSideMetrologyPage(CameraWindow& camerasvc, ViewDirection viewdir) ;
+
+  QWidget* createPhotoBoothPage(CameraWindow& camerasvc, ViewDirection viewdir) ;
+  
 }
 
 #endif
